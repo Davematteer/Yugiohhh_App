e47d6206dc0b41269ec14d2e7e95e2133cead2ed
Davematteer <davematteer@gmail.com>
2025-01-04 23:39:17 +0000

    still playing around with frontend

--- front_end/lib/main.dart
+++ front_end/lib/main.dart
@@ -1,44 +1,17 @@
 import 'package:flutter/material.dart';
 
-class MyBox extends StatelessWidget{
-
-  final Widget word;
-
-  const MyBox({required this.word, super.key});
-
-  Widget build(BuildContext context){
-    return Center(
-      child: Container(
-        height: 100,
-        width: 100,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: Colors.yellow[800],
-            boxShadow:const [BoxShadow(
-                          color:Colors.blue,
-                          blurRadius: 10,
-                          spreadRadius: 2)
-            ] 
-            ),
-
-      ),
-    );
-  }
+void main(){
+  runApp(const MaterialApp(
+    home: Layout()
+  ));
 }
 
-void main(){
-  runApp( MaterialApp(
-    debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title:Text('Bruh',textDirection: TextDirection.ltr,),
-          backgroundColor: const Color.fromRGBO(33, 150, 243, 1),
-          shadowColor: Colors.red,
-          leading: IconButton(
-            onPressed: (){},
-            icon: const Icon(Icons.menu),
-            tooltip: 'Menu',)
-          ),
-        body:const MyBox(word: Text('Yolo'))),
-  ));
+class Layout extends StatelessWidget{
+  const Layout({super.key});
+
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: ,
+    );
+  }
 }
