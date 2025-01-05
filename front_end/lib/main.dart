c1a1358eb7153154c0b298eba1485ac6347b8385
Davematteer <davematteer@gmail.com>
2025-01-05 22:59:15 +0000

    still playing around with frontend

--- front_end/lib/main.dart
+++ front_end/lib/main.dart
@@ -4,29 +4,44 @@ void main(){
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: MyApp()
+        body: Center(
+          child: Counter()
+        )
       )
     )
   );
 }
 
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Counter extends StatefulWidget{
+  const Counter({super.key});
+
+  @override 
+  State<Counter> createState() => _counterState();
+
+}
+
+class _counterState extends State<Counter>{
+
+  int _count = 0;
+
+  void _increment(){
+    setState(() {
+      _count++;
+    });
+  }
 
   @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: ()=>print('yo'),
-      child: Center(
-        child: Container(
-          width: 100,
-          height: 100,
-          decoration: BoxDecoration(color: Colors.blue, 
-                                  borderRadius: BorderRadius.circular(10)),
-        ),
-      )
-
+  Widget build(BuildContext context){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton(
+          onPressed: _increment,
+          child: const Text('Increment')),
+          const SizedBox(width: 16,),
+          Text('Count: $_count')
+      ],
     );
   }
 }
