62b063bc04c51e06bc049af879d4e1d91a39ac97
Davematteer <davematteer@gmail.com>
2025-01-05 01:57:08 +0000

    still playing around with frontend

--- front_end/lib/main.dart
+++ front_end/lib/main.dart
@@ -11,19 +11,38 @@ class Layout extends StatelessWidget{
 
   Widget build(BuildContext context){
     return Scaffold(
-      body: Row(children: [
-        Container(
+      body: Center(
+        child: Container(
+          height: 500,
+          width: 600,
+          
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            ),
-          child: ClipRRect(
-            borderRadius:BorderRadius.circular(10),
-            child: Image.asset('front_end/lib/images/102 Cool iPhone Wallpapers & Aesthetic Backgrounds.jpg',
-            fit: BoxFit.cover,),
+          borderRadius: BorderRadius.circular(10),
+          color: const Color.fromARGB(246, 255, 255, 255),
+          boxShadow: const [
+            BoxShadow(color: Color.fromARGB(255, 158, 158, 158),
+                      spreadRadius: 4,
+                      blurRadius: 5,
+                      )
+            ],
           ),
-          ),
-        Column(),
-      ],)
+          child: Row(children: [
+            Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                ),
+              child: ClipRRect(
+                
+                borderRadius:BorderRadius.circular(10),
+                child: Image.asset('lib/images/102 Cool iPhone Wallpapers & Aesthetic Backgrounds.jpg',
+                fit: BoxFit.cover,),
+              ),
+              ),
+            Column(),
+          ],),
+        ),
+      )
     );
   }
 }
