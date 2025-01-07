b9cfefb3a651d60b7d9f1448a802f0024e8cee60
Davematteer <davematteer@gmail.com>
2025-01-07 17:40:26 +0000

    still playing around with frontend

--- front_end/lib/main.dart
+++ front_end/lib/main.dart
@@ -1,34 +1,67 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main(){
-  runApp(const MyApp());
+  runApp(
+    const MyApp()
+    );
 }
 
-class MyApp extends StatelessWidget{
+// // Section for testing futures
+// Future<http.Response> fetchAlbum(){
+//   return http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+// } 
+
+// // creation of object to map json 
+// class Album{
+//   final int userId;
+//   final int id;
+//   final String title;
+
+//   const Album({
+//     required this.userId,
+//     required this.id,
+//     required this.title}
+//     );
+  
+//   factory Album.fromJson(Map<String,dynamic> json){
+//     return switch(json){
+//       {
+//         'userId': int userId,
+//         'id': int id,
+//         'title': String title,
+//       } =>
+//       Album(
+//         userId: userId, 
+//         id: id, 
+//         title: title),
+//         _ => throw const FormatException('Failed to load Album')
+//     };
+//   }
+// }
+
+
+
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
-      title: 'Flutter App',
-      theme: CupertinoThemeData(
-        brightness: Brightness.light,
-      ),
-      home: CupertinoPageScaffold(
-        navigationBar: CupertinoNavigationBar(
-          backgroundColor: CupertinoColors.systemGrey,
-          middle: Text('Flutter layout demo'),
-        ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Hello niggas')
-          ],),),
-      ),
-        
+    return  MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            height: 100,
+            width: 100,
+            child: Text(''),
+          ),
+        )
+      )
     );
   }
 }
+
+
+
+Future<http.Response> get = http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
 
